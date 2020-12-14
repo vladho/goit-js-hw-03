f5a9e2209edfff0ee479cd5f7920673da7a00491
vladho <vladhopilo@gmail.com>
2020-12-14 16:32:55 +0200

    upd

---
+++ js/task-1.js
@@ -0,0 +1,25 @@
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+// const changeUser = function (user) {
+//    let {mood, hobby, premium } = user;
+//     mood = "happy";
+//     hobby = "skydiving";
+//     premium = false;
+
+//     return {...user, mood, hobby, premium};
+// }
+
+const changeUser = function (user) {
+    user.mood = "happy"
+    user.hobby = "skydiving";
+    user.premium = false;
+    return user;
+}
+
+console.table(changeUser(user));
+console.table(user);
---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+// Напиши функцию countProps(obj), считающую кол-во свойств в объекте. Функция возвращает число - количество свойств.
+
+// const countProps = function(obj) {
+//   // твой код
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(countProps({})); // 0
+
+// console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
+const countProps = function(obj) {
+    Object.keys(obj)
+    console.log(obj)
+    console.log(obj.length)
+};
+
+console.log(countProps({ name: 'Mango', age: 2 }))
