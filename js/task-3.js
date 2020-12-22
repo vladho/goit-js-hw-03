e0baf9a324448e7aae4ddabd1b222cb56f1a06b7
vladho <vladhopilo@gmail.com>
2020-12-22 14:01:13 +0200

    upd

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,25 @@
 const findBestEmployee = function (employees) {
-    let best = 0;
-    let nameOfBest;
-    for (let i = 0; i < Object.values(employees).length; i += 1) {
-        if (Object.values(employees)[i] > best) {
-            best = Object.values(employees)[i];
-            nameOfBest = Object.keys(employees)[i]
+  let best = 0;
+  let nameOfBest;
+  let keysOfArr = Object.keys(employees)
+  let valuesOfArr = Object.values(employees)
+
+  
+    for (let i = 0; i < keysOfArr.length; i += 1) {
+        if (valuesOfArr[i] > best) {
+            best = valuesOfArr[i];
+            nameOfBest = keysOfArr[i]
         }
     }
     return nameOfBest
 };
 
+
+
 console.log(
   findBestEmployee({
     lux: 147,
-    david: 221,
+    david: 229,
     kiwi: 19,
     chelsy: 3
   }),
